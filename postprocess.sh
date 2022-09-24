dfeec2d66e06b934f86a5cd192f57641a4a4704b
Peter J Slack <pslack@pjslack.com>
2022-09-24 11:21:13 -0600

    remove DS_Store adding documentation

--- postprocess.sh
+++ postprocess.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# get a date stamp for use in git push and updates
 DATE=`date`
 
 echo "******* post-processing *****"
-# let's get the latest git repo
+
 env
+# WORKSPACE is passed from maven build
 cd "${WORKSPACE}"
+# commit all changes , this will commit the changes from both any new data
+# from the data submodule and also updates from our java processor
 git commit -am "maven build source update ${DATE}"
 git push
 
--- preprocess.sh
+++ preprocess.sh
@@ -2,10 +2,14 @@
 
 echo "******* preprocessing *****"
 # let's get the latest git repo
+# workspace is passed from the maven builder
+#
 env
 cd "${WORKSPACE}"
 
+# get the latest from our git just in case
 git pull origin main
+# this updates the remote to the latest data
 git submodule foreach git pull origin main
 
 
