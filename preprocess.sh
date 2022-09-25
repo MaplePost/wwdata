4aecf6b0a1f36de26b1dda327b4dcce42bd9880d
Peter J Slack <pslack@pjslack.com>
2022-09-24 21:17:44 -0600

    test code for seeing if we need to update data

--- preprocess.sh
+++ preprocess.sh
@@ -5,6 +5,27 @@ echo "******* preprocessing *****"
 # workspace is passed from the maven builder
 #
 env
+
+
+
+cd "${WORKSPACE}/rawdata/PHESD"
+# some codes to test if we need to pull
+# just for show at this time
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
+
 cd "${WORKSPACE}"
 
 # get the latest from our git just in case
