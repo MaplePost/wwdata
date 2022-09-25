27ee3a9801e2229ca23c79f3f040f59fbe38e453
Peter J Slack <pslack@pjslack.com>
2022-09-24 21:20:01 -0600

    remove test code

--- preprocess.sh
+++ preprocess.sh
@@ -6,26 +6,6 @@ echo "******* preprocessing *****"
 #
 env
 
-
-
-cd "${WORKSPACE}/rawdata/PHESD"
-# some codes to test if we need to pull
-# just for show at this time
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
-else
-    echo "Diverged"
-fi
-
 cd "${WORKSPACE}"
 
 # get the latest from our git just in case
